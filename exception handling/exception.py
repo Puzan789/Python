a3af958210f2628d86804ded8b620c7db528395e
puzan789 <puzan936@gmail.com>
2022-03-21 15:29:09 +0545

    some update

--- exception handling/exception.py
+++ exception handling/exception.py
@@ -1,30 +1,33 @@
 
-# ##########Exceptional Handling
-# try...except
+# # ##########Exceptional Handling
+# # try...except
 
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can't divide by zero!")
-
-
-# try...except...else
-
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can't divide by zero!")
-else:
-    print("You got it right!")
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("You can't divide by zero!")
 
 
-# try...except...else...finally
+# # try...except...else
 
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You can't divide by zero!")
-else:
-    print("You got it right!")
-finally:
-    print("hello i am finally and i will always get executed")
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("You can't divide by zero!")
+# else:
+#     print("You got it right!")
+
+
+# # try...except...else...finally
+
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("You can't divide by zero!")
+# else:
+#     print("You got it right!")
+# finally:
+#     print("hello i am finally and i will always get executed")
+a=[1,2,3]
+b=[1,2,3]
+a==b
